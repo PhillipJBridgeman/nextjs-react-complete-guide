5fee0180a083c67381dc9ccbcb7b051a5bc641ec
Phillip Bridgeman <phillipbridgeman@gmail.com>
2025-01-21 08:42:29 -0600

    Moved files for better organization and added image slide show component.

--- bit_developer_training/nextjs_react_training/section_3_nextjs_essentials/foodies-starting-project/05-onwards-foodies-starting-project/components/main-header/main-header.js
+++ bit_developer_training/nextjs_react_training/section_3_nextjs_essentials/foodies-starting-project/05-onwards-foodies-starting-project/components/main-header/main-header.js
@@ -2,12 +2,15 @@ import Link from "next/link";
 import Image from 'next/image';
 import logoImg from '@/assets/logo.png';
 import classes from './main-header.module.css';
+import MainHeaderBackground from "./main-header-background";
 
 export default function MainHeader() {
     return (
+        <>
+        <MainHeaderBackground />
         <header className={classes.header}>
             <Link className={classes.logo} href="/">
-                <Image src={logoImg} alt='A Plate with food on it.' />
+                <Image src={logoImg} alt='A Plate with food on it.' priority />
                 NextLevel Food
             </Link>
 
@@ -18,5 +21,6 @@ export default function MainHeader() {
                 </ul>
             </nav>
         </header>
+        </>
     );
 }
