ebebcae5dced1a7782f9f7d02c5764cf45b80f02
Phillip Bridgeman <phillipbridgeman@gmail.com>
2025-01-21 08:17:56 -0600

    Update to styling for main-header.js component with module CSS.

--- bit_developer_training/nextjs_react_training/section_3_nextjs_essentials/foodies-starting-project/05-onwards-foodies-starting-project/components/main-header.js
+++ bit_developer_training/nextjs_react_training/section_3_nextjs_essentials/foodies-starting-project/05-onwards-foodies-starting-project/components/main-header.js
@@ -1,18 +1,19 @@
 import Link from "next/link";
+import Image from 'next/image';
 import logoImg from '@/assets/logo.png';
+import classes from './main-header.module.css';
 
 export default function MainHeader() {
     return (
-        <header>
-            <Link href="/">
-                <img src={logoImg.src} alt='A Plate with food on it.' />
+        <header className={classes.header}>
+            <Link className={classes.logo} href="/">
+                <Image src={logoImg} alt='A Plate with food on it.' />
+                NextLevel Food
             </Link>
 
-            <nav>
+            <nav className={classes.nav}>
                 <ul>
                     <li><Link href="/meals">Browse Meals</Link></li>
-                </ul>
-                <ul>
                     <li><Link href="/community">Foodies Community</Link></li>
                 </ul>
             </nav>
