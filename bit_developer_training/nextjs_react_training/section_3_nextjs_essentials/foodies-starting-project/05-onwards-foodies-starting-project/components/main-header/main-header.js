076e6660b48f24e76dec4565cebfd4b69a07e447
Phillip Bridgeman <phillipbridgeman@gmail.com>
2025-01-21 11:08:05 -0600

    Added Meals sections to project.

--- bit_developer_training/nextjs_react_training/section_3_nextjs_essentials/foodies-starting-project/05-onwards-foodies-starting-project/components/main-header/main-header.js
+++ bit_developer_training/nextjs_react_training/section_3_nextjs_essentials/foodies-starting-project/05-onwards-foodies-starting-project/components/main-header/main-header.js
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import Image from 'next/image';
+import NavLink from "../nav-link";
 import logoImg from '@/assets/logo.png';
 import classes from './main-header.module.css';
 import MainHeaderBackground from "./main-header-background";
@@ -16,8 +17,12 @@ export default function MainHeader() {
 
             <nav className={classes.nav}>
                 <ul>
-                    <li><Link href="/meals">Browse Meals</Link></li>
-                    <li><Link href="/community">Foodies Community</Link></li>
+                    <li>
+                        <NavLink href="/meals"> Browse Meals</NavLink>
+                    </li>
+                    <li>
+                        <NavLink href="/community">Foodies Community</NavLink>
+                    </li>
                 </ul>
             </nav>
         </header>
