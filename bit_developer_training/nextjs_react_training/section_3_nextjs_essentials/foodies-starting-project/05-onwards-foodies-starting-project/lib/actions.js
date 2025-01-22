f97e3aabc41da9fac683879bcc379e0494b4a8dc
Phillip Bridgeman <phillipbridgeman@gmail.com>
2025-01-22 11:53:22 -0600

    New section along with updating to foodies project.

--- bit_developer_training/nextjs_react_training/section_3_nextjs_essentials/foodies-starting-project/05-onwards-foodies-starting-project/lib/actions.js
+++ bit_developer_training/nextjs_react_training/section_3_nextjs_essentials/foodies-starting-project/05-onwards-foodies-starting-project/lib/actions.js
@@ -1,7 +1,12 @@
 'use server';
 import { saveMeal } from './meals';
+import { redirect } from 'next/navigation';
 
-export async function shareMeal(formData) {
+function isInvaildtext(text) {
+    return !text || text.trim() === '';
+}
+
+export async function shareMeal(prevState, formData) {
     const meal = {
         title: formData.get('title'),
         summary: formData.get('summary'),
@@ -10,6 +15,23 @@ export async function shareMeal(formData) {
         creator: formData.get('name'),
         creator_email: formData.get('email')
     }
+
+    if (
+        isInvaildtext(meal.title) ||
+        isInvaildtext(meal.summary) ||
+        isInvaildtext(meal.instructions) ||
+        isInvaildtext(meal.creator) ||
+        isInvaildtext(meal.creator_email) ||
+        !meal.creator_email.includes('@') ||
+        !meal.image || meal.image.size === 0
+    )
+    {
+        return {
+            message: 'Invalid input.'
+        };
+    }
     
     await saveMeal(meal);
+    revalidatePath('/meals', 'layout');
+    redirect('/meals');
 }
